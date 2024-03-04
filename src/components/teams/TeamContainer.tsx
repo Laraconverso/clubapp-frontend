f2aea707e49a29c520c3da512b414134ee318f8c
Pablo Alvarez <pipualvarez1@gmail.com>
2024-03-04 15:34:37 -0300

    bloque de imagenes

--- src/components/teams/TeamContainer.tsx
+++ src/components/teams/TeamContainer.tsx
@@ -3,11 +3,11 @@ import Team from "./Team"
 
 const TeamContainer = () => {
   return (
-    <div  className={`bg-baltic-sea-900 py-5 rounded-3xl lg:max-w-md`}>
+    <div  className={`bg-baltic-sea-900 px-4 py-5 rounded-3xl`}>
       <section>
-        <h2 className={`text-center text-2xl font-bauhs text-primary-500`}>EQUIPOS</h2>
+        <h2 className={`text-center text-xl font-bauhs text-primary-500`}>EQUIPOS</h2>
       </section>
-      <section>
+      <section className="h-40 content-center">
         <Team/>
       </section>
     </div>
